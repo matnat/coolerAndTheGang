2fa8e844ac4682fae7dc9170c8429f89485fc0ff
NatPro <NatPro@meinPC>
2013-05-15 22:44:46 +0200

    image test

--- Frogger/src/view/MainView.java
+++ Frogger/src/view/MainView.java
@@ -1,8 +1,35 @@
 package view;
 
-public class MainView {
-	
-	
-	
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.JFrame;
+
+public class MainView extends JFrame {
+
+	public static void main(String[] args) {
+		MainView spiel = new MainView();
+	}
+
+	Image bild;
+
+	MainView() {
+
+		super("Mein Frogger");
+		this.bild = java.awt.Toolkit.getDefaultToolkit().getImage(
+				"smiley.gif");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(200, 300);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+	//	g.drawLine(50, 50, 200, 200);
+		g.drawImage(bild, 10, 10, null);
+
+	}
 }
